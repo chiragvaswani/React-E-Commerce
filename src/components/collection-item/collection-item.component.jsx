8089d3e1aabc1b1af6481eb89ea19b8e79c19f6a
chiragvaswani <chiragvaswani751@gmail.com>
2021-03-19 16:12:46 +0530

    Added footer to the collection item that shows the name and price of the product. Also exported to component.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -9,6 +9,12 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       style={{
         backgroundImage: `url(${imageUrl})`,
       }}
-    ></div>
+    />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
   </div>
 );
+
+export default CollectionItem;
