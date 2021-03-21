41bc44bbaadbf340f6127bd67ae46e99bb3e9097
chiragvaswani <chiragvaswani751@gmail.com>
2021-03-21 16:13:28 +0530

    Used the firebase authentication in sign-in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.styles.scss";
 
 class SignIn extends Component {
@@ -53,6 +54,9 @@ class SignIn extends Component {
           />
           <CustomButton type="submit" value="Submit Form">
             Sign In
+          </CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            Sign in with Google
           </CustomButton>
         </form>
       </div>
