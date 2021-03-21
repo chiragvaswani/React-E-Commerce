56dd75bb4fd2754e7ba50b0b3cc964621aedefd8
chiragvaswani <chiragvaswani751@gmail.com>
2021-03-21 17:12:22 +0530

    Wrapped all the buttons and sent a property isGoogleSignIn for a button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,12 +52,15 @@ class SignIn extends Component {
             required="required"
             label="Password"
           />
-          <CustomButton type="submit" value="Submit Form">
-            Sign In
-          </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+
+          <div className="buttons">
+            <CustomButton type="submit" value="Submit Form">
+              Sign In
+            </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
