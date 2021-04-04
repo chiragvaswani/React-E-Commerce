c2a8d42bcb639a0300ac3954965fb484dcd41dd3
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-04 12:24:30 +0530

    Stored user data in the database upon OAuth

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -20,9 +20,9 @@ class App extends React.Component {
   componentDidMount() {
     // onAuthStateChanged takes a function as a parameter. The parameter of that function is the state of the user on auth or on our application
     // This connection is always open until the component is unmounted
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    // It establishes a connection between the application anf Firebase
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      createUserProfileDocument(userAuth);
     });
   }
 
