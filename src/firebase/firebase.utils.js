59bba91a5748417ebeec12b59a28bb536131450c
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-05 20:17:51 +0530

    Used the CartDropdown component in the header

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,12 +1,49 @@
 import firebase from "firebase/app";
 // We can access firestore / auth by using the firebase we imported above
-import "firebase/firestore";
+import "firebase/firestore"; // think of this as file paths
 import "firebase/auth";
 
-const config = {};
+const config = {
+  apiKey: "AIzaSyDWWPA-SMEZHJd0hjlwUxxJrJOKeF6lrPw",
+  authDomain: "crwn-db-4352d.firebaseapp.com",
+  projectId: "crwn-db-4352d",
+  storageBucket: "crwn-db-4352d.appspot.com",
+  messagingSenderId: "217903184783",
+  appId: "1:217903184783:web:d3302705fe7dcebd8369fa",
+  measurementId: "G-99E04047WM",
+};
+
+// This function is used to store the user's details in the database
+// Async because we're making an API request
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; // Don't do anything if the user has logged out
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  console.log(userRef);
+  const snapShot = await userRef.get();
+  console.log(snapShot);
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error creating the user", error);
+    }
+  }
+  return userRef;
+};
 
 firebase.initializeApp(config); // Initialise the application with the above configuration
 
+// Export the auth and firestore methods
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
