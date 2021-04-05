f49544704aa35ac53c0847e6d31858ca5b80eb22
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-05 12:13:12 +0530

    Wrapped the Provider around BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux"; // Component we get from react redux which should wrap the entire application
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </Provider>,
   document.getElementById("root")
 );
