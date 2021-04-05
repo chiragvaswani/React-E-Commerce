34f04a05e3255bbe359976b28c2f7f5a2b2e16d3
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-05 19:56:43 +0530

    Referenced UserActionTypes object instead of using a hardcoded string

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,6 @@
 // Action creator functions -> Functions that return objects that are in the correct format that the actions are supposed to be in
-
+import { UserActionTypes } from "./user.types";
 export const setCurrentUser = (user) => ({
-  type: "SET_CURRENT_USER",
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
