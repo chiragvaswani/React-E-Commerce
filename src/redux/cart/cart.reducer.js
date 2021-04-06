9b086794e7eb1db0323e06d7bad9944a41dc7ee7
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-06 16:57:09 +0530

    Using the addItemToCart function to get array of objects and mapping it to cartItems property

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,6 @@
+import { addItem } from "./cart.actions";
 import CartActionTypes from "./cart.types";
+import addItemToCart from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -16,7 +18,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
 
     default:
