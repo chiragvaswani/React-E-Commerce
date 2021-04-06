a4108a54d8f7dc5166255010be5d81770f355880
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-06 16:19:04 +0530

    Defined mapDispatchToProps to use the addItems action to add items in the cartItems array

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 
 import "./collection-item.styles.scss";
-
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
 import CustomButton from "../custom-button/custom-button.component";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+  const { imageUrl, name, price } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
-    <CustomButton inverted>Add to cart</CustomButton>
-  </div>
-);
+  );
+};
+// This component will get the addItem function as a prop.
+// Then we'll use this function by giving it the item argument that'll be the item we added to the cart
+// The addItem function will be executed with the item we passed in
+// This function is an action so it'll return an object with a type and payload
+// Then dispatch function will pass in that object with regular redux flow i.e. the reducer
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
 
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
