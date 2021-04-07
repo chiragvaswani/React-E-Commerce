86d4565424ffaa65ef86c7508b4edfef8624d048
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-07 19:55:51 +0530

    Used selectCartItemsCount to enable memoization

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./cart-icon.styles.scss";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
-
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
@@ -17,12 +17,9 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-// itemCount is a selector
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-    0
-  ),
+// Why passing the state? Because the selectCartItemsCount will pass the state to selectCartItems which will pass it to selectCart. Then we'll get the cart, then from the cart we'll get the cartItems and from the cartItems we'll get the count
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
