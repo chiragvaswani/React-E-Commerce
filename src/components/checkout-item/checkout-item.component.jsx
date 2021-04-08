09ce89e3dc4a2a0c1830a313f2010bd039ebcc34
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-08 17:28:35 +0530

    Connect the component to the store and use the action clearItemFromCart to remove the item from cart when the user clicks on the cross icon

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,17 +2,30 @@ import React from "react";
 
 import "./checkout-item.styles.scss";
 
-// Will be passing the entire item instead of parts of it as props because we need the user to be able to add and remove items and the action to add items need the entire item as payload
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
-    </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+import { connect } from "react-redux";
 
-export default CheckoutItem;
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
+
+// Will be passing the entire item instead of parts of it as props because we need the user to be able to add and remove items and the action to add items need the entire item as payload
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
