2737bdcdbdf9a4aff56cc1726d2421179b589b90
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-11 19:32:20 +0530

    Create persistor using persistStore from redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger"; // logger middleware
+import { persistStore } from "redux-persist"; // allows our browser to cache the store depending upon the configuration options
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
