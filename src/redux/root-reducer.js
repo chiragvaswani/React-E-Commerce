e025645b87b1e1797e6c8956ec3f825a2209183a
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-11 19:43:01 +0530

    Define config object for and use it with the root reducer and the persistReducer function to persist the root reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,10 +2,23 @@
 // Base reducer that represents state of the entire application
 
 import { combineReducers } from "redux";
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // gives the localStorage object from the window object
+
+// Possible configurations for redux persist -
+const persistConfig = {
+  key: "root", // At what point inside the reducer object do we want to store everything
+  storage, // storage: storage where the storage value is the localStorage that we imported
+  whitelist: ["cart"], // list consisting of string names of the reducers that we wish to store
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
