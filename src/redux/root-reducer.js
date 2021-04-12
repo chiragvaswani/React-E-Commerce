adc4ea5f8c79acfc1ed8706c6476d58ca58ef891
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-12 09:46:08 +0530

    Add the shop reducer in the root reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // gives the localStorage object from the window object
@@ -21,6 +22,7 @@ const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
