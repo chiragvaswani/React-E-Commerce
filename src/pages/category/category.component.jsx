d3829914603f9472be991ecf6b2c3de3e06c8184
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-12 10:17:46 +0530

    Define the category page component that gets the match object as a param and can use it to get the categoryId

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./category.styles.scss";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+const CategoryPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className="category">
+      <h2>CATEGORY</h2>
+    </div>
+  );
+};
+
+export default CategoryPage;
