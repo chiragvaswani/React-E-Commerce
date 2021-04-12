bbc722a58b988916d1c13e2508eb62d5c88f05ee
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-12 10:44:13 +0530

    Pass the props of the component to mapStateToProps and use selector to get the collection based on the param in the url

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,15 +2,24 @@ import React from "react";
 
 import "./collection.styles.scss";
 
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 const CollectionPage = ({ match }) => {
-  console.log(match);
+  console.log(match.params.collectionId);
   return (
     <div className="collection-page">
       <h2>COLLECTION</h2>
     </div>
   );
 };
+
+// ownProps here are the props that the CollectionPage component gets
+// selectCollection(ownProps.match.params.collectionId) returns another function ie a selector and then we pass the state to that selector
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
 
 export default CollectionPage;
