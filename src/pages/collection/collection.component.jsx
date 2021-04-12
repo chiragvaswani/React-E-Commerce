ff34f7394d6ee45933cde5aeacdcb6f8c1b226d3
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-12 16:05:03 +0530

    Render the title of the collection and the corresponding items

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,11 +7,17 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>COLLECTION</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
