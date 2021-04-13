d69529997bf40d8f011cafeb11b65b9430623f45
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-13 13:58:29 +0530

    Define a selector that returns an array of collections from the normalised data

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,6 +8,16 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+// Since SHOP_DATA is now an object, we cannot map over it. So we define a new selector
+// that takes in the normalised data object, gets all the keys out of it, and returns an array with
+// all the items of that particular key so that collections-overview component can iterate
+// over the array of collections and render CollectionPreview component by mapping on the
+// array of collections
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 // To select one collection based on the provided collection id
 // This function returns another function
 export const selectCollection = (collectionUrlParam) =>
