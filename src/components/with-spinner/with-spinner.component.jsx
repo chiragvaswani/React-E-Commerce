23a7e65b7e3455f3328835844b3643c6acbdf4d9
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-17 13:36:58 +0530

    Add extra comment

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -4,6 +4,7 @@ import { SpinnerOverlay, SpinnerContainer } from "./with-spinner.styles";
 
 // WithSpinner is a HOC that returns a component that takes in some props (one of which is isLoading)
 // Then, depending upon the value of isLoading, the component either returns a spinner component or the original component with all the other props
+// But overall, we are returning the component that takes the isLoading and otherProps as arguments
 const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
   return isLoading ? (
     <SpinnerOverlay>
