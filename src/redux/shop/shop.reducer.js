736dcffc7652c150fa188eaf99a30783c421bfad
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-19 15:26:36 +0530

    Added isFetching and errorMessage to the initial state. Defined case for asynchronous action of fetching data from firestore

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -3,14 +3,28 @@ import ShopActionTypes from "./shop.types";
 
 const INITIAL_STATE = {
   collections: null,
+  isFetching: false, // tells if we're fetching data
+  errorMessage: "", // used to store the error encountered while fetching data
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ShopActionTypes.UPDATE_COLLECTIONS:
+    case ShopActionTypes.FETCH_COLLECTIONS_START:
       return {
         ...state,
+        isFetching: true,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
         collections: action.payload,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload, // the payload will hold the error message
       };
     default:
       return state;
