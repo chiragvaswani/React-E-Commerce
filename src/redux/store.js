2b351d47406c51517eca623b2c64393ee1efd4ac
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-19 14:45:40 +0530

    Imported thunk from 'redux-thunk' and added it in the middlewares array

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger"; // logger middleware
 import { persistStore } from "redux-persist"; // allows our browser to cache the store depending upon the configuration options
-
+import thunk from "redux-thunk";
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
+const middlewares = [thunk];
 
 // Use the logger only when the application is run locally
 if (process.env.NODE_ENV === "development") middlewares.push(logger);
