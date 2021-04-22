d77409b1ae5ba4a8e39dbb9c4954e778a8557940
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-22 10:08:54 +0530

    Define a saga that uses takeEvery to listen for FETCH_COLLECTIONS_START and invokes a generator function that logs something to the console

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,14 @@
+// Holds all the saga code related to the shop
+import { takeEvery } from "redux-saga/effects";
+import ShopActionTypes from "./shop.types"; // Because we're gonna listen for specific action types
+
+export function* fetchCollectionsAsync() {
+  yield console.log("I am fired");
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
