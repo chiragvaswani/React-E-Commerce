2128672809c6eeb82c70e45875dc05c6fcf04ba0
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-22 10:09:43 +0530

    Import createSagaMiddleware to add the saga middleware Import fetchCollectionsStart saga and run it using the middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger"; // logger middleware
 import { persistStore } from "redux-persist"; // allows our browser to cache the store depending upon the configuration options
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "./root-reducer";
 
-const middlewares = [thunk];
+import { fetchCollectionsStart } from "./shop/shop.sagas";
+
+const sagaMiddleware = createSagaMiddleware(); // this function can take an object with certain configuration settings
+
+const middlewares = [sagaMiddleware];
 
 // Use the logger only when the application is run locally
 if (process.env.NODE_ENV === "development") middlewares.push(logger);
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart); // write each individual saga in here
+
 export const persistor = persistStore(store);
 
 export default { store, persistor };
