f897e24f3958f7c711f4758189fd55b024cae295
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-23 17:50:27 +0530

    Use common action for sign in success and failure with email and google. Also, remove setCurrentUser action.

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,9 +1,5 @@
 // Action creator functions -> Functions that return objects that are in the correct format that the actions are supposed to be in
 import UserActionTypes from "./user.types";
-export const setCurrentUser = (user) => ({
-  type: UserActionTypes.SET_CURRENT_USER,
-  payload: user,
-});
 
 // We don't need a payload here as the saga will handle all the stuff and this is just for notfying the saga to begin
 export const googleSignInStart = () => ({
@@ -11,14 +7,14 @@ export const googleSignInStart = () => ({
 });
 
 // Return the user
-export const googleSignInSuccess = (user) => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+export const signInSuccess = (user) => ({
+  type: UserActionTypes.SIGN_IN_SUCCESS,
   payload: user,
 });
 
 // Return the error if any
-export const googleSignInFailure = (error) => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+export const signInFailure = (error) => ({
+  type: UserActionTypes.SIGN_IN_FAILURE,
   payload: error,
 });
 
@@ -26,16 +22,4 @@ export const googleSignInFailure = (error) => ({
 export const emailSignInStart = (emailAndPassword) => ({
   type: UserActionTypes.EMAIL_SIGN_IN_START,
   payload: emailAndPassword,
-});
-
-// Return the user
-export const emailSignInSuccess = (user) => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
-  payload: user,
-});
-
-// Return the error if any
-export const emailSignInFailure = (error) => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
-  payload: error,
 });
