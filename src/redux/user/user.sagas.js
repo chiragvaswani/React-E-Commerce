4ecefa03162db35fd8157744a9e7b0bfd8232b16
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-23 16:27:27 +0530

    Create a file that contains all the user sagas. Define a saga that listens for the action which is dispatched when the user signs in using google and the corresponding function that should be executed

---
+++ src/redux/user/user.sagas.js
@@ -0,0 +1,23 @@
+import { takeLatest, put } from "redux-saga/effects";
+
+import UserActionTypes from "./user.types";
+
+import {
+  auth,
+  googleProvider,
+  createUserProfileDocument,
+} from "../../firebase/firebase.utils";
+
+// We're not using the function to do signInWithPopup from firebase.utils because we need the value that is returned when signInWithPopup is executed
+export function* signInWithGoogle() {
+  try {
+    const userRef = yield auth.signInWithPopup(googleProvider);
+    console.log(userRef);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export function* onGoogleSignInStart() {
+  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START);
+}
