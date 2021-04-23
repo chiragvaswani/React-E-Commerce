006079593a92008dde0180c5bd31d405d8702685
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-23 16:53:50 +0530

    Call the createUserProfileDocument function to sign the user in and dispatch the googleSignInSuccess action

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -7,12 +7,18 @@ import {
   googleProvider,
   createUserProfileDocument,
 } from "../../firebase/firebase.utils";
+import { googleSignInSuccess } from "./user.actions";
 
 // We're not using the function to do signInWithPopup from firebase.utils because we need the value that is returned when signInWithPopup is executed
 export function* signInWithGoogle() {
   try {
-    const userRef = yield auth.signInWithPopup(googleProvider);
-    console.log(userRef);
+    const { user } = yield auth.signInWithPopup(googleProvider);
+    const userRef = yield call(createUserProfileDocument, user);
+    const userSnapshot = yield userRef.get();
+    // Dispatch the success action
+    yield put(
+      googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
+    );
   } catch (error) {
     console.log(error.message);
   }
