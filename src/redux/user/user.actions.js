cce2cb932de6feed890eb717c8f15499e473aafd
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-23 16:13:58 +0530

    Export the action types object by default and change the import accordingly in the reducer and action files

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,5 @@
 // Action creator functions -> Functions that return objects that are in the correct format that the actions are supposed to be in
-import { UserActionTypes } from "./user.types";
+import UserActionTypes from "./user.types";
 export const setCurrentUser = (user) => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
