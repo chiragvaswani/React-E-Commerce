669e460237bb11f62d9d7ba1918517500e9251bf
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-23 16:10:11 +0530

    Define the action functoins for email and google sign in start, success, and failure

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -4,3 +4,38 @@ export const setCurrentUser = (user) => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
+
+// We don't need a payload here as the saga will handle all the stuff and this is just for notfying the saga to begin
+export const googleSignInStart = () => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_START,
+});
+
+// Return the user
+export const googleSignInSuccess = (user) => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+// Return the error if any
+export const googleSignInFailure = (error) => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+  payload: error,
+});
+
+// We'll need the email and password for the sign in
+export const emailSignInStart = (emailAndPassword) => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START,
+  payload: emailAndPassword,
+});
+
+// Return the user
+export const emailSignInSuccess = (user) => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+// Return the error if any
+export const emailSignInFailure = (error) => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
+  payload: error,
+});
