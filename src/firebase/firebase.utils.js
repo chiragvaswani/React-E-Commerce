3769e8e8af3e8fcc446cdc757392ff25b333790d
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-26 14:49:14 +0530

    Define a utility function that returns a promise which resolves wiht the value of userAuth and uses the observer observable pattern

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -76,6 +76,15 @@ firebase.initializeApp(config); // Initialise the application with the above con
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsibscribe = auth.onAuthStateChanged((userAuth) => {
+      unsibscribe(); // unsubscribe as soon as we're done checking for the user
+      resolve(userAuth);
+    }, reject);
+  });
+};
+
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 googleProvider.setCustomParameters({ prompt: "select_account" }); // We want to always trigger the goolge pop up whenever we use google auth provider for authentication and sign in
 export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
