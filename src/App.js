c6d70fa7cb7357623c5d06a372d75f6873d4a884
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-26 14:51:51 +0530

    Dispatch the action that checks current user in componentDidMount lifecycle method

--- src/App.js
+++ src/App.js
@@ -12,28 +12,13 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from "./components/header/header.component";
 
 import { selectCurrentUser } from "./redux/user/user.selector";
+import { checkUserSession } from "./redux/user/user.actions";
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    // onAuthStateChanged takes a function as a parameter. The parameter of that function is the state of the user on auth or on our application
-    // This connection is always open until the component is unmounted
-    // It establishes a connection between the application anf Firebase
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(
-    //   async (userAuth) => {
-    //     if (userAuth) {
-    //       const userRef = await createUserProfileDocument(userAuth);
-    //       // Listen to the userRef for the changes in the data. But, we also get the first state of the data which in this case is "snapShot"
-    //       userRef.onSnapshot((snapShot) => {
-    //         setCurrentUser({
-    //           id: snapShot.id, // Getting the id from the snapshot
-    //           ...snapShot.data(), // Getting the data from the snapshot
-    //         });
-    //       });
-    //     } else setCurrentUser(userAuth); // Setting the current user to null in the state if the user logs out
-    //   },
-    //   (error) => console.log(error)
-    // );
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -69,4 +54,8 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+  checkUserSession: () => dispatch(checkUserSession()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
