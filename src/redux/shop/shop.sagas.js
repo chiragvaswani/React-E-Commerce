7420436862838ab3a2ea1ae9554f914e6850fd94
chiragvaswani <chiragvaswani751@gmail.com>
2021-04-28 09:44:24 +0530

    Define a base saga that calls the fetchCollectionsStart saag

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,5 +1,5 @@
 // Holds all the saga code related to the shop
-import { takeLatest, call, put } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "redux-saga/effects";
 import ShopActionTypes from "./shop.types"; // Because we're gonna listen for specific action types
 
 import {
@@ -32,4 +32,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
