201b2bb34c0fc06ee36c7c683ad8b0264f2ddf87
chiragvaswani <chiragvaswani751@gmail.com>
2021-05-03 10:51:12 +0530

    Serve static file for production and listen for specified port

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+  );
+}
+
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log(`Server running on ${port}`);
+});
